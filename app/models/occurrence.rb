fc326b1b4044500bd9ee5d09eae9cbd7214426c4
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-08-02 22:19:15 -0300

    validations added to occurrence model

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,6 +1,9 @@
 class Occurrence < ApplicationRecord
   belongs_to :user
   belongs_to :ship
+
+  validates :latitude, :longitude, :date, :time, :ship_id, presence: true
+
   include PgSearch::Model
   pg_search_scope :search_by_origin_and_destination,
     against: %i[ship_origin ship_destination],
