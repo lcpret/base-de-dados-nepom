34d91b7903124d97db9aa90a39c7656d00ef5473
lcpret <lcpret@gmail.com>
2022-08-07 22:32:11 -0300

    change validation

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,7 +3,7 @@ class Ship < ApplicationRecord
   has_many :occurrences
   has_many :members, through: :crews
   has_one_attached :photo
-  validates :name, :ship_type, :application, presence: true
+  validates :name, presence: true
   validates :registration, uniqueness: true, allow_blank: true
 
   include PgSearch::Model
